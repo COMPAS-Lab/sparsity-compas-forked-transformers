6ca6f4a2d545d8d3aec842df0b6bb38bd038d45a
Tianchu Ji <tianchu.ji@stonybrook.edu>
2023-03-04 13:34:27 -0500

    add bfp share block size control

--- src/transformers/models/opt/modeling_opt.py
+++ src/transformers/models/opt/modeling_opt.py
@@ -136,6 +136,7 @@ class OPTAttention(nn.Module):
         is_cross_attention = key_value_states is not None
 
         bsz, tgt_len, _ = hidden_states.size()
+        bfp_cell_size = 80
 
         # get query proj
         query_states = self.q_proj(hidden_states) * self.scaling
@@ -176,8 +177,8 @@ class OPTAttention(nn.Module):
 
         src_len = key_states.size(1)
 
-        BFP_query_states = bfp_ops.convert_bfp(query_states, 4, query_states.shape[-1])
-        BFP_key_states = bfp_ops.convert_bfp(key_states, 4, key_states.shape[-1])
+        BFP_query_states = bfp_ops.convert_bfp(query_states, 4, bfp_cell_size)
+        BFP_key_states = bfp_ops.convert_bfp(key_states, 4, bfp_cell_size)
 
         attn_weights = torch.bmm(query_states, key_states.transpose(1, 2))
         # attn_weights = torch.bmm(BFP_query_states, BFP_key_states.transpose(1, 2))
@@ -208,7 +209,7 @@ class OPTAttention(nn.Module):
             attn_weights = nn.functional.softmax(attn_weights, dim=-1)
 
         # apply static pruning
-        # attn_weights = torch.where(attn_weights>1e-3, attn_weights, 0.)
+        attn_weights = torch.where(attn_weights>5e-4, attn_weights, 0.)
 
         if layer_head_mask is not None:
             if layer_head_mask.size() != (self.num_heads,):
@@ -232,8 +233,9 @@ class OPTAttention(nn.Module):
             attn_weights_reshaped = None
 
         attn_probs = nn.functional.dropout(attn_weights, p=self.dropout, training=self.training)
-
-        # attn_probs = bfp_ops.convert_bfp(attn_probs, 4, attn_probs.shape[-1])
+        # attn_probs = bfp_ops.convert_bfp(attn_probs, 4, bfp_cell_size)
+        if output_attentions:
+            attn_weights_reshaped = attn_probs
 
         attn_output = torch.bmm(attn_probs, value_states)
 
