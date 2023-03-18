c9d0e39edee9c9e24a551f774d2d99a878681fd8
ydshieh <ydshieh@users.noreply.github.com>
2023-03-18 09:49:38 +0100

    update script

--- utils/update_tiny_models.py
+++ utils/update_tiny_models.py
@@ -33,6 +33,14 @@ def get_all_model_names():
 
 
 def get_tiny_model_summary():
+
+    special_models = [
+        "EncoderDecoderModel-bert-bert",
+        "SpeechEncoderDecoderModel-wav2vec2-bert",
+        "VisionEncoderDecoderModel-vit-gpt2",
+        "VisionTextDualEncoderModel-vit-bert",
+    ]
+
     # All tiny model base names on Hub
     model_names = get_all_model_names()
     models = hf_api.list_models(
@@ -49,17 +57,16 @@ def get_tiny_model_summary():
         model = model.replace("tiny-random-", "")
         if not model[0].isupper():
             continue
-        if model not in model_names:
+        if model not in model_names or model not in special_models:
             continue
         _models.add(model)
 
-    models = sorted(_models)
-
     # All tiny model names on Hub
     summary = {}
-    for model in models[:]:
-        content = {"tokenizer_classes": set(), "processor_classes": set(), "model_classes": set()}
+    for model in models:
         repo_id = f"hf-internal-testing/tiny-random-{model}"
+        model = model.split("-")[0]
+        content = {"tokenizer_classes": set(), "processor_classes": set(), "model_classes": set()}
         try:
             time.sleep(1)
             tokenizer_fast = AutoTokenizer.from_pretrained(repo_id)
@@ -109,6 +116,8 @@ def get_tiny_model_summary():
 
 
 if __name__ == "__main__":
+    get_tiny_model_summary()
+    exit(0)
     # All model names defined in auto mappings
     model_names = set(get_all_model_names())
 
@@ -121,7 +130,7 @@ if __name__ == "__main__":
     # Remove a tiny model name if one of its framework implementation hasn't yet a tiny version on the Hub.
     not_on_hub = model_names.difference(tiny_models_names)
     existing_model_ = set()
-    for model_name in copy(tiny_models_names):
+    for model_name in copy.copy(tiny_models_names):
         if not model_name.startswith("TF") and f"TF{model_name}" in not_on_hub:
             tiny_models_names.remove(model_name)
         elif model_name.startswith("TF") and model_name[2:] in not_on_hub:
