beb05a472ef16fcb5781ac9b3d31aad91302cde3
ydshieh <ydshieh@users.noreply.github.com>
2023-03-18 09:10:06 +0100

    update script

--- utils/update_tiny_models.py
+++ utils/update_tiny_models.py
@@ -1,122 +1,137 @@
-from huggingface_hub import hf_api, ModelFilter
-import transformers
-from transformers import AutoTokenizer, AutoImageProcessor, AutoFeatureExtractor, AutoProcessor
+import copy
 import json
 import time
-from transformers.image_processing_utils import BaseImageProcessor
-
-model_names = set()
-# Each auto modeling files contains multiple mappings. Let's get them in a dynamic way.
-for module_name in ["modeling_auto", "modeling_tf_auto", "modeling_flax_auto"]:
-    module = getattr(transformers.models.auto, module_name, None)
-    if module is None:
-        continue
-    # all mappings in a single auto modeling file
-    mapping_names = [x for x in dir(module) if x.endswith("_MAPPING_NAMES") and x.startswith("MODEL_")]
-    for name in mapping_names:
-        mapping = getattr(module, name)
-        if mapping is not None:
-            for v in mapping.values():
-                if isinstance(v, (list, tuple)):
-                    model_names.update(v)
-                elif isinstance(v, str):
-                    model_names.add(v)
-
-model_names = sorted(model_names)
-print(model_names)
-
-models = hf_api.list_models(filter=ModelFilter(author="hf-internal-testing",))
-_models = set()
-for x in models:
-    model = x.modelId
-    org, model = model.split("/")
-    if not model.startswith("tiny-random-"):
-        continue
-    model = model.replace("tiny-random-", "")
-    if not model[0].isupper():
-        continue
-    if model not in model_names:
-        continue
-    _models.add(model)
-
-models = sorted(_models)
-sorted(models)
-
-summary = {}
-for model in models[:]:
-    content = {"tokenizer_classes": set(), "processor_classes": set(), "model_classes": set()}
-    repo_id = f"hf-internal-testing/tiny-random-{model}"
-    try:
-        time.sleep(1)
-        tokenizer_fast = AutoTokenizer.from_pretrained(repo_id)
-        content["tokenizer_classes"].add(tokenizer_fast.__class__.__name__)
-    except:
-        pass
-    try:
-        time.sleep(1)
-        tokenizer_slow = AutoTokenizer.from_pretrained(repo_id, use_fast=False)
-        content["tokenizer_classes"].add(tokenizer_slow.__class__.__name__)
-    except:
-        pass
-    try:
-        time.sleep(1)
-        img_p = AutoImageProcessor.from_pretrained(repo_id)
-        content["processor_classes"].add(img_p.__class__.__name__)
-    except:
-        pass
-    try:
-        time.sleep(1)
-        feat_p = AutoFeatureExtractor.from_pretrained(repo_id)
-        if not isinstance(feat_p, BaseImageProcessor):
-            content["processor_classes"].add(feat_p.__class__.__name__)
-    except:
-        pass
-    # try:
-    #     p = AutoProcessor.from_pretrained(repo_id)
-    # except:
-    #     pass
-    try:
-        time.sleep(1)
-        model_class = getattr(transformers, model)
-        m = model_class.from_pretrained(repo_id)
-        content["model_classes"].add(m.__class__.__name__)
-    except:
-        pass
-    try:
-        time.sleep(1)
-        model_class = getattr(transformers, f"TF{model}")
-        m = model_class.from_pretrained(repo_id)
-        content["model_classes"].add(m.__class__.__name__)
-    except:
-        pass
-    content["tokenizer_classes"] = sorted(content["tokenizer_classes"])
-    content["processor_classes"] = sorted(content["processor_classes"])
-    content["model_classes"] = sorted(content["model_classes"])
-
-    summary[model] = content
-    with open("model_summary.json", "w") as fp:
-        json.dump(summary, fp, ensure_ascii=False, indent=4)
-
-exit(0)
-
-import json
 
 from create_dummy_models import create_tiny_models
+from huggingface_hub import ModelFilter, hf_api
+
+import transformers
+from transformers import AutoFeatureExtractor, AutoImageProcessor, AutoTokenizer
+from transformers.image_processing_utils import BaseImageProcessor
+
+
+def get_all_model_names():
+    model_names = set()
+    # Each auto modeling files contains multiple mappings. Let's get them in a dynamic way.
+    for module_name in ["modeling_auto", "modeling_tf_auto", "modeling_flax_auto"]:
+        module = getattr(transformers.models.auto, module_name, None)
+        if module is None:
+            continue
+        # all mappings in a single auto modeling file
+        mapping_names = [x for x in dir(module) if x.endswith("_MAPPING_NAMES") and x.startswith("MODEL_")]
+        for name in mapping_names:
+            mapping = getattr(module, name)
+            if mapping is not None:
+                for v in mapping.values():
+                    if isinstance(v, (list, tuple)):
+                        model_names.update(v)
+                    elif isinstance(v, str):
+                        model_names.add(v)
+
+    model_names = sorted(model_names)
+    return model_names
+
+
+def get_tiny_model_summary():
+    # All tiny model base names on Hub
+    model_names = get_all_model_names()
+    models = hf_api.list_models(
+        filter=ModelFilter(
+            author="hf-internal-testing",
+        )
+    )
+    _models = set()
+    for x in models:
+        model = x.modelId
+        org, model = model.split("/")
+        if not model.startswith("tiny-random-"):
+            continue
+        model = model.replace("tiny-random-", "")
+        if not model[0].isupper():
+            continue
+        if model not in model_names:
+            continue
+        _models.add(model)
+
+    models = sorted(_models)
+
+    # All tiny model names on Hub
+    summary = {}
+    for model in models[:]:
+        content = {"tokenizer_classes": set(), "processor_classes": set(), "model_classes": set()}
+        repo_id = f"hf-internal-testing/tiny-random-{model}"
+        try:
+            time.sleep(1)
+            tokenizer_fast = AutoTokenizer.from_pretrained(repo_id)
+            content["tokenizer_classes"].add(tokenizer_fast.__class__.__name__)
+        except:
+            pass
+        try:
+            time.sleep(1)
+            tokenizer_slow = AutoTokenizer.from_pretrained(repo_id, use_fast=False)
+            content["tokenizer_classes"].add(tokenizer_slow.__class__.__name__)
+        except:
+            pass
+        try:
+            time.sleep(1)
+            img_p = AutoImageProcessor.from_pretrained(repo_id)
+            content["processor_classes"].add(img_p.__class__.__name__)
+        except:
+            pass
+        try:
+            time.sleep(1)
+            feat_p = AutoFeatureExtractor.from_pretrained(repo_id)
+            if not isinstance(feat_p, BaseImageProcessor):
+                content["processor_classes"].add(feat_p.__class__.__name__)
+        except:
+            pass
+        try:
+            time.sleep(1)
+            model_class = getattr(transformers, model)
+            m = model_class.from_pretrained(repo_id)
+            content["model_classes"].add(m.__class__.__name__)
+        except:
+            pass
+        try:
+            time.sleep(1)
+            model_class = getattr(transformers, f"TF{model}")
+            m = model_class.from_pretrained(repo_id)
+            content["model_classes"].add(m.__class__.__name__)
+        except:
+            pass
+        content["tokenizer_classes"] = sorted(content["tokenizer_classes"])
+        content["processor_classes"] = sorted(content["processor_classes"])
+        content["model_classes"] = sorted(content["model_classes"])
+
+        summary[model] = content
+        with open("model_summary.json", "w") as fp:
+            json.dump(summary, fp, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
+    # All model names defined in auto mappings
+    model_names = set(get_all_model_names())
+
     with open("tests/utils/tiny_model_summary.json") as fp:
         tiny_model_info = json.load(fp)
+    tiny_models_names = set()
+    for model_base_name in tiny_model_info:
+        tiny_models_names.update(tiny_model_info[model_base_name]["model_classes"])
 
-    tiny_models_on_hub = set()
-    for name in tiny_model_info:
-        tiny_models_on_hub.update(tiny_model_info[name]["model_classes"])
-    existing_model_classes = sorted(tiny_models_on_hub)
+    # Remove a tiny model name if one of its framework implementation hasn't yet a tiny version on the Hub.
+    not_on_hub = model_names.difference(tiny_models_names)
+    existing_model_ = set()
+    for model_name in copy(tiny_models_names):
+        if not model_name.startswith("TF") and f"TF{model_name}" in not_on_hub:
+            tiny_models_names.remove(model_name)
+        elif model_name.startswith("TF") and model_name[2:] in not_on_hub:
+            tiny_models_names.remove(model_name)
+    tiny_models_names = sorted(tiny_models_names)
 
     output_path = "tiny_models"
     all = True
     model_types = None
-    models_to_skip = existing_model_classes
+    models_to_skip = tiny_models_names
     no_check = True
     upload = False
     organization = "hf-internal-testing"
