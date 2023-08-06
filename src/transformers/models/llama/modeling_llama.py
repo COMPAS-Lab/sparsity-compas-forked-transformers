6d10c3d5c2ab5e948c682bb8aec9ae161680adad
Tianchu Ji <tianchu.ji@stonybrook.edu>
2023-08-05 23:49:45 -0400

    add bfp toggler for llama and opt, add sparsity computation

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -447,7 +447,7 @@ class LlamaAttention(nn.Module):
         attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
         attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
         # apply static pruning
-        # attn_weights = torch.where(attn_weights>2e-3, attn_weights, 0.)
+        # attn_weights = torch.where(attn_weights>1e-2, attn_weights, 0.)
         if toggle:
             attn_weights = bfp_ops.convert_bfp(attn_weights, 4, vec_size, entire=entire)
 
